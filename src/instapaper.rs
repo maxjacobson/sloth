ea47f5dbfc4638d634420e8d2cd6028565bde201
Max Jacobson <max@hardscrabble.net>
2016-12-24 14:35:18 -0500

    Start setting headers to make more correct request

--- src/instapaper.rs
+++ src/instapaper.rs
@@ -2,6 +2,9 @@ use config::SlothConfig;
 extern crate toml;
 use std::collections::BTreeMap;
 extern crate hyper;
+use std::io::Read;
+use std::time::UNIX_EPOCH;
+use std::hash::{Hash, Hasher, SipHasher};
 
 pub struct InstapaperApp {
     consumer_key: String,
@@ -49,12 +52,30 @@ impl InstapaperApp {
                            self.username,
                            self.password);
         let mut headers = hyper::header::Headers::new();
+        // FIXME: don't unwrap
+        let timestamp = UNIX_EPOCH.elapsed().unwrap().as_secs();
+        let oauth_nonce = InstapaperApp::hash(format!("{}", timestamp));
+        headers.set(
+            hyper::header::Authorization(format!("OAuth oauth_nonce=\"{}\", oauth_signature_method=\"HMAC-SHA1\", oauth_consumer_key=\"{}\", oauth_timestamp=\"{}\"", oauth_nonce, self.consumer_key, timestamp))
+        );
 
         let post_request = client.post("https://instapaper.com/api/1/oauth/access_token");
-        // Currently getting a forbidden response!
+        // Currently getting a forbidden response! because missing an oauth_signature
         match post_request.headers(headers).body(&body).send() {
-            Ok(response) => println!("{:?}", response),
-            Err(err) => println!("{:?}", err),
+            Ok(mut response) => {
+                let mut response_body = String::new();
+                println!("OK: {:?}", response);
+                response.read_to_string(&mut response_body).unwrap();
+                println!("Body: {:?}", response_body);
+            },
+            Err(err) => println!("ERR: {:?}", err),
         }
+    }
+
+    fn hash(input: String) -> String {
+        // FIXME: this hasher is deprecated (although the docs pointed me to it so shrug)
+        let mut s = SipHasher::new();
+        input.hash(&mut s);
+        format!("{}", s.finish())
     }
 }
